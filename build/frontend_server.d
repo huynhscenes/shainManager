a00eae703aec3e90fb2a432c433ca4d166ad6035
huynhscenes <huynh@scenes.co.jp>
2019-06-10 18:13:55 +0900

    no message

---
+++ build/frontend_server.d
@@ -0,0 +1,1 @@
+frontend_server.d: /Users/huynhnguyen/Desktop/flutter/bin/cache/artifacts/engine/darwin-x64/frontend_server.dart.snapshot
---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <firebase_auth/FirebaseAuthPlugin.h>
+#import <firebase_core/FirebaseCorePlugin.h>
+#import <firebase_database/FirebaseDatabasePlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTFirebaseAuthPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseAuthPlugin"]];
+  [FLTFirebaseCorePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseCorePlugin"]];
+  [FLTFirebaseDatabasePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseDatabasePlugin"]];
+}
+
+@end
